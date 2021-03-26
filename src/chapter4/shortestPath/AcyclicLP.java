bc852c652906ee04f8ad4e7bb11e89480f24d900
stevenqiu <568606697@qq.com>
2021-03-26 16:23:32 +0800

    add AcyclicLP and CPM

---
+++ src/chapter4/shortestPath/AcyclicLP.java
@@ -0,0 +1,53 @@
+package chapter4.shortestPath;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class AcyclicLP {
+    private final DirectedEdge[] edgeTo;
+    private final double[] distTo;
+
+    AcyclicLP(EdgeWeightedDigraph G, int s) {
+        edgeTo = new DirectedEdge[G.V()];
+        distTo = new double[G.V()];
+
+        for (int v = 0; v < G.V(); ++v) {
+            distTo[v] = Double.NEGATIVE_INFINITY;
+        }
+        distTo[s] = 0.0;
+
+        TopologicalEWD top = new TopologicalEWD(G);
+        for (int v : top.order()) {
+            relax(G, v);
+        }
+    }
+
+    // 顶点的松弛
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) {
+            // 边的松弛
+            int w = e.to();
+            if (distTo[w] < distTo[v] + e.weight()) {
+                distTo[w] = distTo[v] + e.weight();
+                edgeTo[w] = e;
+            }
+        }
+    }
+
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] > Double.NEGATIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Deque<DirectedEdge> path = new LinkedList<>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.push(e);
+        }
+        return path;
+    }
+}
